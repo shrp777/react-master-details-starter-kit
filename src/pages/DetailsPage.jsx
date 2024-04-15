94c48e14257c288a2c34aeb55f2d37a262ebd815
Shrp <alex@sherpa.one>
2024-04-15 16:22:20 +0200

    commentaires

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -3,20 +3,36 @@ import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function DetailsPage() {
+  //récupération dynamique de l'id renseigné dans l'URI
   const { id } = useParams();
 
   const [task, setTask] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchDataFromAPI(id) {
       try {
+        setIsError(false);
+        //affichage du message de chargement
+        setIsLoading(true);
+
+        //requête auprès de l'API
         const response = await axios.get(
           "https://jsonplaceholder.typicode.com/todos/" + id
         );
 
+        //affectation des données récupérées via l'API
         setTask(response.data);
+
+        setIsError(false);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
+        //affichage du message d'erreur
+        setIsError(true);
+
+        setIsLoading(false);
       }
     }
 
@@ -26,6 +42,8 @@ export default function DetailsPage() {
   return (
     <div>
       <h1>Details {id}</h1>
+      {isLoading && <p>Chargement en cours</p>}
+      {isError && <p>Une erreur s'est produite</p>}
       {task && <h2>{task.title}</h2>}
       <Link to="/">Retour à la page Master</Link>
     </div>
